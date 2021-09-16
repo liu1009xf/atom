b9143472ab14ca416dc7a0dc48634fdd4c68a0e8
Xifan Liu <jimmy@Xifans-MacBook-Pro.local>
2021-09-17 08:59:22 +0900

    fix namespace

--- test/datetime_test.cpp
+++ test/datetime_test.cpp
@@ -21,15 +21,15 @@ public:
 };
 
 TEST_F(DateTimeTest, strTimepointConversion) {
-    auto t1 = atom::util::clock::detail::str2Timepoint(this->st1);
-    auto t2 = atom::util::clock::detail::timepoint2Str(t1);
+    auto t1 = atom::util::tp::detail::str2Timepoint(this->st1);
+    auto t2 = atom::util::tp::detail::timepoint2Str(t1);
     EXPECT_EQ(st1, t2);
 }
 
 TEST_F(DateTimeTest, timepointComparasion) {
-    auto t1 = atom::util::clock::detail::str2Timepoint(st1);
-    auto t2 = atom::util::clock::detail::str2Timepoint(st2);
-    auto t3 = atom::util::clock::detail::str2Timepoint(st3);
+    auto t1 = atom::util::tp::detail::str2Timepoint(st1);
+    auto t2 = atom::util::tp::detail::str2Timepoint(st2);
+    auto t3 = atom::util::tp::detail::str2Timepoint(st3);
     ASSERT_GT(t2, t1);
     ASSERT_GT(t1, t3);
     ASSERT_GT(t2, t3);
