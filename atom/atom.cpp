1463df458ced7b378d9f6b3b94ff1fdc6a96fcb6
Xifan Liu <jimmy@Xifans-MacBook-Pro.local>
2021-10-05 22:23:01 +0900

    revise doc of atom namespace

--- atom/atom.cpp
+++ atom/atom.cpp
@@ -1,16 +1,5 @@
 #include <atom/atom.hpp>
 
-/** @file
- * @brief This is a "brief" explaination of this file
- *
- * This is a "Detailed" explaination of this file
- */
-
- /**
-  * @brief Brief explaination of namespace MyNamespace
-  *
-  * Detailed explaination of namespace MyNamespace
-  */
 namespace atom
 {
     std::string atom_version()
